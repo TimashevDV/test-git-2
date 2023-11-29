0654e2ea51ed64714d0b9d717305b976aa4e0cf1
Dmitry <Timashev_Dima@mail.ru>
2023-11-29 21:11:38 +0400

    made changes in master

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-console.log('Hello');
-
 console.log('Hello world');
+
+const userAge = 35;
+if (userAge >= 18) {
+    console.log('Вы совершеннолетний');
+} else {
+    console.log('Вы НЕ совершеннолетний');
+}
